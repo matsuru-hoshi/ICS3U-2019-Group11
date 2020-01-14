229092bc8586ccedebf00a279fdc96d7cf517907
Matsuru Hoshi <matsuru.hoshi@mths.ca>
2020-01-14 19:46:21 +0000

    Added signin, signout, profile and temp

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_DlSthRzmh', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: 'fdma68pj4kj65qhc43t2fpm1p' //is this used anywhere?
+    },
+};
