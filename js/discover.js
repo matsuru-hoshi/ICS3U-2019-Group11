42a0aef9a2eff96d46cc246c140178e3b9a625a3
Matsuru Hoshi <matsuru.hoshi@mths.ca>
2020-01-22 19:52:17 +0000

    everything

--- js/discover.js
+++ js/discover.js
@@ -9,16 +9,18 @@ async function getUserNumber(email_address) {
   console.log(api_data);
   
   console.log(api_data);
-  return parseInt(api_data);
+  var chocolate = api_data['body'];
+  return chocolate;
 }
 
 async function returnUserNumber(email_address) {
   //Passes user email to getUserNumber()
   var chocolate_number = await getUserNumber(email_address);
+  console.log(typeof chocolate_number);
         
   //get the user info from API Gate
 
-  const api_url = 'https://rf79scj6x0.execute-api.us-east-1.amazonaws.com/prod/update-number?user_email=' + email_address + '&chocolate_number=' + chocolate_number;
+  const api_url = 'https://rf79scj6x0.execute-api.us-east-1.amazonaws.com/prod/update-number?user_email=' + email_address + '&chocolate_number=' + chocolate_number; 
   const api_response = await fetch(api_url);
   const api_data = await(api_response).json();
   console.log(api_data);
